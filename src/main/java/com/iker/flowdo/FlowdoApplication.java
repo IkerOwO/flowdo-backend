1cde4d8e5697e1f8ccffd1290d30da7e67e21186
unknown <csiker12@gmail.com>
2026-09-12 15:25:44 +0200

    Initial commit

---
+++ src/main/java/com/iker/flowdo/FlowdoApplication.java
@@ -0,0 +1,13 @@
+package com.iker.flowdo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowdoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlowdoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iker/flowdo/FlowdoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iker.flowdo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowdoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
